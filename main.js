444ea83659fc9ad116a50a92798c551b4bc8548d
klubent37 <33526508+klubent37@users.noreply.github.com>
2020-07-27 23:31:31 +0300

    main.js
    движок к сайту

---
+++ main.js
@@ -0,0 +1,9 @@
+Ext.onReady(function() {
+ var ceLinks = Ext.select("UL[id=navbar] LI");
+ ceLinks.on("mouseover", function(e, t) {
+ Ext.get(this).addClass("hovered");
+ });
+ ceLinks.on("mouseout", function(e, t) {
+ Ext.get(this).removeClass("hovered");
+ });
+});
